bd6f23261249aa569ef453f634452c1494a217aa
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2022-12-08 17:12:50 +0400

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,181 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="ThOME">
+   <div class="Survey">
+    <p>Complete a survey to get 1 year of luck</p>
+   </div>
+   <h1>{{title}}</h1>
+</div>
+
+ <div v-if="showModal1">
+  //slot example instead of [props]
+  <Modal @close="toggleModal1">
+
+    <div v-for="title in titles" :key="title">
+      <p>{{title.rdr}}</p>
+    </div>
+
+    <template v-slot:ratings>
+      <div v-for="value in RDR" :key="value">
+        <li>{{value.val}}</li>
+      </div>
+    </template>
+
+    <template v-slot:map>
+      <a href="a">Map images</a>
+    </template>
+  </Modal>
+ </div>
+
+
+ <div v-if="showModal2">
+  <Modal @close="toggleModal2">
+    <h1>modal 2 h1 </h1>
+    <p>Modal 2 paragrapht</p>  
+    <img src="./assets/logo.png" style="width:50px" />
+    <a href="a">Logdddd up</a>
+
+    <template v-slot:ratings>
+      <div v-for="value in SVN" :key="value">
+        <li>{{value.val}}</li>
+      </div>
+    </template>
+    
+   
+  </Modal>
+ </div>
+
+
+<section class="grid">
+
+ <button style="background-image:url(https://cdn.sanity.io/images/e7woiqxp/production/6c088b7f878c304a7058a3fb51ca76ea8b70b149-1232x822.jpg?w=2000&fit=max&auto=format)"
+ @click="toggleModal1">RIveduremp</button>
+
+ <button style="background-image:url(https://cdn.sanity.io/images/e7woiqxp/production/2361c8951baad05401955038d2b893053b030485-1243x1303.jpg?w=2000&fit=max&auto=format)"
+ @click="toggleModal2">Savamme</button>
+
+ <button style="background-image:url(https://cdn.sanity.io/images/e7woiqxp/production/c1e1d5ef19744aba3bff6b66ffdf6055c0cb39ec-2048x875.jpg?w=2000&fit=max&auto=format)"
+ @click="toggleModal3">Port LOiuis</button>
+
+ <button style="background-image:url(https://cdn.sanity.io/images/e7woiqxp/production/21b390df9a8f8d77d6e5ee8107c4c6359bb3de34-1100x732.jpg?w=2000&fit=max&auto=format)"
+ @click="toggleModal4">plaine will</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal5">Modal 5</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal6">Modal 6</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal7">Modal 7</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal8">Modal 8</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal9">Modal 9</button>
+
+ <button style="background-image:url()"
+ @click="toggleModal10">Modal 10</button>
+
+</section>
+
+
+<footer>
+  <div>
+    <h3>Footer content</h3>
+  </div>
+</footer>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  components:{Modal},
+
+  data(){
+      return{
+
+        title:'Molocal version2',
+        titles:[],
+        RDR:[],
+        SVN:[],
+
+        showModal1: true,
+        showModal2 : false,
+        showModal3 : false,
+        showModal4 : false,
+        showModal5 : false,
+        showModal6 : false,
+        showModal7 : false,
+        showModal8 : false,
+        showModal9 : false,
+        showModal10 : false,
+      }
+    },
+
+    mounted(){
+      fetch('http://localhost:3000/titles')
+      .then(res => res.json())
+      .then(data => this.titles = data)
+      .catch(err => console.log(err.message))
+
+      fetch('http://localhost:3000/RDR')
+      .then(res => res.json())
+      .then(data => this.RDR = data)
+
+      fetch('http://localhost:3000/SVN')
+      .then(res => res.json())
+      .then(data => this.SVN = data)
+    },
+
+
+    methods:{
+      toggleModal1() {
+        this.showModal1 = !this.showModal1
+        
+      },
+      toggleModal2(){
+        this.showModal2 = !this.showModal2
+
+      },
+      toggleModal3(){
+        this.showModal3 = !this.showModal3
+
+      },
+      toggleModal4(){
+        this.showModal4 = !this.showModal4
+
+      },
+      toggleModal5(){
+        this.showModal5 = !this.showModal5
+
+      },
+      toggleModal6(){
+        this.showModal6 = !this.showModal6
+
+      },
+      toggleModal7(){
+        this.showModal7 = !this.showModal7
+
+      },
+
+      toggleModal8(){
+        this.showModal8 = !this.showModal8
+
+      },
+      toggleModal9(){
+        this.showModal9 = !this.showModal9
+
+      },
+      toggleModal10(){
+        this.showModal10 = !this.showModal10
+      }
+    }
   }
-}
 </script>
 
 <style>
@@ -20,7 +184,89 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
 }
+.imagetop{
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-color: rebeccapurple;
+}
+.imagetop p{
+  z-index: 2;
+  font-size: 4rem;
+  color:aquamarine;
+}
+.ThOME{
+  /*background-image:url("https://source.unsplash.com/random/") ;*/
+  background-position: center;
+  color:white;
+  width: 100vw;
+  height: 50vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+.Survey{
+  width:200px;
+  height:200px;
+  background-color: #222222;
+  color:tomato;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  cursor: pointer;
+}
+.Survey p{
+  font-size: 1.2rem;
+}
+
+.grid{
+  width: 100vw;
+  height:100%;
+  background-color: #181818;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+.grid button {
+width: 600px;
+height: 250px;
+cursor: pointer;
+border-radius: 15px;
+background-repeat: no-repeat;
+background-position: center;
+background-size: cover;
+}
+
+footer{
+  background-color: aqua;
+}
+
+/*input range Slider bad good great */
+
+
+
+@media only screen and (max-width: 600px){
+  .grid button {
+    width:350px
+  }
+  .Survey{
+    margin-left: 20px;
+  }
+}
+
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <p class="fakeclosebtn" @click="closeModal">X</p>
+        <div class="modal">
+
+
+            <div class="imgtop">
+             <slot></slot>
+            </div>
+        
+        
+        <section class="clmrw">
+
+        <div class = "rate">
+            <div class="fixedrate">
+                    <li>🏆 Overall Score</li>
+                    <li>❤️ Liked by residents</li>
+                    <li>🦺 Safety</li>
+                    <li>🚦 Traffic density</li>
+                    <li>🙃 Happiness</li>
+                    <li>👪 Population</li>
+                    <li>🌡️ Temperature</li>
+                    <li>🏢 Place to work</li>
+                    <li>🥳 Fun / Nightlife</li>
+                    <li>🚶 Walkability</li>
+            </div>
+
+
+            <div class="variblerate">
+            <slot name="ratings">
+             
+            </slot>
+
+        </div>
+        </div>
+
+        <div class="maps">
+            <slot name="map"></slot>
+        </div>
+
+        </section>
+
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+    props:[ ],
+
+    methods : {
+        closeModal () {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.modal{
+    width:65vw;
+    height: 90vh;
+    padding: 0px;
+    margin: 20px auto;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap:10px;
+}
+
+.imgtop{
+    width:100%;
+    height: 25vh;
+    background-color:pink;
+    border-radius: 15px;
+    box-sizing: border-box;
+}
+.clmrw{
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+    justify-content: space-evenly;
+    gap:10px;    
+}
+.rate{
+    display: flex;
+    flex-direction: row;
+    width:100%;
+    height:100%;
+    background-color: white;
+    border-radius:15px;
+    box-sizing: border-box;
+    padding: 10px;
+}
+.maps{
+    width: 100%;
+    height: 100%;
+    background: blue;
+    border-radius: 15px;
+    box-sizing: border-box;
+}
+.fixedrate{
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+.fixedrate li{
+    list-style: none;
+    font-size: 1.2rem;
+    text-align: center;
+}
+
+.backdrop {
+    top:0;
+    position: fixed;
+    background: rgba(0,0,0,0.7);
+    width: 100%;
+    height: 100%;
+}
+.fakeclosebtn{
+    position: fixed;
+    color: white;
+    font-size:2rem;
+    right:20px;
+    top:10px;
+    cursor:pointer;
+    font-weight: bold;
+}
+@media only screen and (max-width: 600px){
+    .modal{
+     width:100vw;
+     height: 100%;
+     margin:0px;
+    }
+    .imgtop{
+        border-radius: 0px;
+    }
+    .clmrw{
+    flex-direction: column;
+    padding: 0px;
+    margin-top:-10px
+    }
+    .maps{
+        display: none;
+    }
+}
+</style>
